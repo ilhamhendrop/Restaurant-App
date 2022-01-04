b36410afdbc23750d46c67c34ba79103eaee6489
ilhamhendrop <ilhamhendroprasetyo@gmail.com>
2022-01-04 10:10:54 +0700

    restaurant app

---
+++ restaurant_app/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:restaurant_app/widget/styles.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Restaurant App',
+      theme: ThemeData(
+        colorScheme: Theme.of(context).colorScheme.copyWith(
+          primary: primaryColor,
+          onPrimary: Colors.black,
+          secondary: secondaryColor,
+        ),
+      ),
+    );
+  }
+}
+
