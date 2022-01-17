5d8ccd68b7835018461821cabf663afc05765a5a
ilhamhendrop <ilhamhendroprasetyo@gmail.com>
2022-01-17 08:47:16 +0700

    Edit

--- restaurant_app/lib/page/search_page.dart
+++ restaurant_app/lib/page/search_page.dart
@@ -16,67 +16,67 @@ class SearchPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       body: SafeArea(
-          child: Padding(
-        padding: MediaQuery.of(context).padding,
-        child: OfflineBuilder(
-          connectivityBuilder: (
-            BuildContext context,
-            ConnectivityResult connectivity,
-            Widget child,
-          ) {
-            final bool connected = connectivity != ConnectivityResult.none;
-            return Stack(
-              fit: StackFit.expand,
-              children: [
-                Positioned(
-                  height: 28.0,
-                  left: 0.0,
-                  right: 0.0,
-                  child: connected
-                      ? Visibility(
-                          visible: true,
-                          child: Container(),
-                        )
-                      : Container(
-                          color: Colors.red,
-                          child: const Center(
-                            child: Text(
-                              "Loss Connection",
+        child: Padding(
+          padding: MediaQuery.of(context).padding,
+          child: OfflineBuilder(
+            connectivityBuilder: (
+              BuildContext context,
+              ConnectivityResult connectivity,
+              Widget child,
+            ) {
+              final bool connected = connectivity != ConnectivityResult.none;
+              return Stack(
+                fit: StackFit.expand,
+                children: [
+                  Positioned(
+                    height: 28.0,
+                    left: 0.0,
+                    right: 0.0,
+                    child: connected
+                        ? Visibility(
+                            visible: true,
+                            child: Container(),
+                          )
+                        : Container(
+                            color: Colors.red,
+                            child: const Center(
+                              child: Text(
+                                "Loss Connection",
+                              ),
                             ),
                           ),
-                        ),
-                ),
-                Center(
-                  child: Padding(
-                    padding: MediaQuery.of(context).padding,
-                    child: Column(
-                      children: [
-                        Expanded(
-                          child: ChangeNotifierProvider(
-                            create: (_) => SearchRestaurantProvider(
-                                searchApiService: ApiService()),
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: const [
-                                SearchWidget(),
-                                SizedBox(height: 20),
-                                Flexible(
-                                  child: RestaurantSearch(),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
-                    )
                   ),
-                ),
-              ],
-            );
-          },
-          child: Container(),
+                  Center(
+                    child: Padding(
+                        padding: MediaQuery.of(context).padding,
+                        child: Column(
+                          children: [
+                            Expanded(
+                              child: ChangeNotifierProvider(
+                                create: (_) => SearchRestaurantProvider(
+                                    searchApiService: ApiService()),
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: const [
+                                    SearchWidget(),
+                                    SizedBox(height: 20),
+                                    Flexible(
+                                      child: RestaurantSearch(),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        )),
+                  ),
+                ],
+              );
+            },
+            child: Container(),
+          ),
         ),
-      )),
+      ),
     );
   }
 }
